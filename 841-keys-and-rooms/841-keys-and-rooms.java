73ce9c217dfa97aacb1bc68b1983076c0925ec72
Prashant Kumar <60420825+Orewa-Prashant@users.noreply.github.com>
2022-04-15 01:18:51 +0530

    Time: 1 ms (95.48%), Space: 42.3 MB (82.67%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.java
+++ 841-keys-and-rooms/841-keys-and-rooms.java
@@ -2,20 +2,20 @@ class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         HashSet<Integer> h=new HashSet<>();
         h.add(0);
-        boolean[] ans=new boolean[]{false};
-        fun(rooms,h,0,ans);
-        return ans[0];
+        //boolean[] ans=new boolean[]{false};
+        fun(rooms,h,0);
+        return h.size()==rooms.size()?true:false;
     }
-    void fun(List<List<Integer>> rooms, HashSet<Integer> h, int room, boolean[] ans){
-        if(h.size()==rooms.size()){
-            ans[0]=true;
-            return;
-        }
+    void fun(List<List<Integer>> rooms, HashSet<Integer> h, int room){
+        // if(h.size()==rooms.size()){
+        //     ans[0]=true;
+        //     return;
+        // }
         
         for(int next:rooms.get(room)){
             if(!h.contains(next)){
                 h.add(next);
-                fun(rooms,h,next,ans);
+                fun(rooms,h,next);
             }
         }
     }
