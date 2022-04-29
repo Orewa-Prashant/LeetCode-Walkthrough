b7ccad4049f5fdf367aebd39b20de9525088c97d
Prashant Kumar <60420825+Orewa-Prashant@users.noreply.github.com>
2022-04-29 17:58:30 +0530

    Time: 9 ms (47.90%), Space: 50 MB (19.35%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,36 +1,35 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-            ArrayList[] graph = new ArrayList[numCourses];
-            for(int i=0;i<numCourses;i++)
-                graph[i] = new ArrayList();
-                
-            boolean[] visited = new boolean[numCourses];
-            boolean[] dp = new boolean[numCourses];
-            for(int i=0; i<prerequisites.length;i++){
-                graph[prerequisites[i][1]].add(prerequisites[i][0]);
-            }
-
-            for(int i=0; i<numCourses; i++){
-                if(!dfs(graph,visited,i, dp))
-                    return false;
-            }
-            return true;
+    public boolean canFinish(int numCourses , int[][] prerequisites) {
+        Map<Integer,List<Integer>> list=new HashMap<>();
+        int n=prerequisites.length;
+        for(int i=0;i<n;i++){
+            if(!list.containsKey(prerequisites[i][0]))
+                list.put(prerequisites[i][0],new ArrayList<Integer>());
+            list.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
-
-        private boolean dfs(ArrayList[] graph, boolean[] visited, int course, boolean[] dp){
-            if(visited[course])
-                return dp[course];
-            else
-                visited[course] = true;
-
-            for(int i=0; i<graph[course].size();i++){
-                if(!dfs(graph,visited,(int)graph[course].get(i), dp)) {
-                    dp[course] = false;
-                    return false;
-                }
-            }
-            
-            dp[course] = true;
-            return true;
+        //System.out.println(list);
+        HashSet<Integer> h=new HashSet<>();
+        boolean[] used=new boolean[numCourses];
+        for(int i=0;i<numCourses;i++){
+            //if(used[i])continue;
+            //System.out.println(i);
+            if(!dfs(list,i,used,h))
+                return false;
+            h.add(i);
+            //System.out.println(i);
         }
+        return true;
+    }
+    
+    boolean dfs(Map<Integer,List<Integer>> list, int node, boolean[] used, HashSet<Integer> h){
+        if(!list.containsKey(node)||h.contains(node))return true;
+        if(used[node])return false;
+        used[node]=true;
+        for(int next:list.get(node)){
+            if(!dfs(list,next,used,h))
+                return false;
+        }
+        used[node]=false;
+        return true;
+    }
 }
