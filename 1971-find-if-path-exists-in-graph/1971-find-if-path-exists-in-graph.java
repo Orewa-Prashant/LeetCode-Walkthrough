390c67a6f90021e0b8b53741a7664828f858ae81
Prashant Kumar <60420825+Orewa-Prashant@users.noreply.github.com>
2022-04-30 22:09:19 +0530

    Time: 320 ms (19.11%), Space: 235.5 MB (17.97%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -20,9 +20,9 @@ class Solution {
         for(int i:list.get(s)){
             if(used[i])continue;
             b=b||dfs(i,d,list,used,dp);
-            if(b)return dp[s]=true;
+            //if(b)return dp[s]=true;
         }
         used[s]=false;
-        return dp[s]=false;
+        return dp[s]=b;
     }
 }
