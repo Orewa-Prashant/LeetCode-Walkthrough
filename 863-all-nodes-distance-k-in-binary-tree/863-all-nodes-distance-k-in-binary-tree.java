e26180d68f15cb64be1d211d343720a81cd97f9d
Prashant Kumar <60420825+Orewa-Prashant@users.noreply.github.com>
2022-05-14 14:02:44 +0530

    Time: 15 ms (54.59%), Space: 43.3 MB (37.44%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> ans=new ArrayList<>();
+        HashMap<TreeNode,List<TreeNode>> map=new HashMap<>();
+        fun(root,null,map);
+        HashSet<TreeNode> used=new HashSet<>();
+        //System.out.println(map);
+        dfs(map,k,ans,target,used);
+        return ans;
+    }
+    void fun(TreeNode root,TreeNode par, HashMap<TreeNode,List<TreeNode>> map){
+        if(root==null)return;
+        if(!map.containsKey(root))
+            map.put(root,new ArrayList<TreeNode>());
+        if(par!=null){
+            map.get(par).add(root);
+            map.get(root).add(par);
+        }
+        fun(root.left,root,map);
+        fun(root.right,root,map);
+        
+    }
+    
+    void dfs(HashMap<TreeNode,List<TreeNode>> map, int k, List<Integer> ans, TreeNode target, HashSet<TreeNode> used){
+        if(k==0){
+            ans.add(target.val);
+            return;
+        }
+        used.add(target);
+        for(TreeNode node:map.get(target)){
+            if(!used.contains(node))
+            dfs(map,k-1,ans,node,used);
+        }
+        used.remove(target);
+    }
+    
+}
