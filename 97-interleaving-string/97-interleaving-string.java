0646606812f618cdb44737d911c6100d2f8ded0c
Prashant Kumar <60420825+Orewa-Prashant@users.noreply.github.com>
2022-05-23 11:26:41 +0530

    Time: 2 ms (86.37%), Space: 42.5 MB (46.24%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,41 +1,34 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        HashMap<String,Boolean> cache=new HashMap<>();
-        
-        return fun(s1,s2,s3,0,0,0,cache);
+        Boolean[][] dp=new Boolean[s1.length()+1][s2.length()+1];
+        return fun(s1,s2,s3,0,0,dp);
     }
-    boolean fun(String a, String b, String c, int i,int j, int ct, HashMap<String,Boolean> cache){
+    boolean fun(String a, String b, String c, int i,int j,Boolean[][] dp){
         if(i+j==c.length()){
             if(i==a.length() && j==b.length())return true;
             return false;
         }
         if(i==a.length() && j==b.length())return false;
-        String inp=i+" "+j+" "+ct;
-        if(cache.containsKey(inp))return cache.get(inp);
+        
+        if(dp[i][j]!=null)return dp[i][j];
+        
         boolean is_a=true,is_b=true;
         if(i==a.length())is_a=false;
         if(j==b.length())is_b=false;
-        boolean ans=false;
         
         if(is_a && is_b)
         if(a.charAt(i)==c.charAt(i+j) && b.charAt(j)==c.charAt(i+j)){
-            ans=fun(a,b,c,i+1,j,ct+1,cache)||fun(a,b,c,i,j+1,ct-1,cache);
-            cache.put(inp,ans);
-            return ans;
+            return dp[i][j]=fun(a,b,c,i+1,j,dp)||fun(a,b,c,i,j+1,dp);
         }
         if(is_a)
         if(a.charAt(i)==c.charAt(i+j)){
-            ans= fun(a,b,c,i+1,j,ct+1,cache);
-            cache.put(inp,ans);
-            return ans;
+            return dp[i][j]=fun(a,b,c,i+1,j,dp);
+
         }
         if(is_b)
         if(b.charAt(j)==c.charAt(i+j)){
-            ans= fun(a,b,c,i,j+1,ct-1,cache);
-            cache.put(inp,ans);
-            return ans;
+            return dp[i][j]=fun(a,b,c,i,j+1,dp);
         }
-        cache.put(inp,ans);
-        return ans;
+        return dp[i][j]=false;
     }
 }
