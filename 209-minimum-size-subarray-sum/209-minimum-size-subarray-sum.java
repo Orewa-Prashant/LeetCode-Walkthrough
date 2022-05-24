4547f501e570028c6b71240d454f6592fc452d3e
Prashant Kumar <60420825+Orewa-Prashant@users.noreply.github.com>
2022-05-24 11:19:32 +0530

    Time: 8 ms (13.10%), Space: 45.9 MB (6.41%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -1,19 +1,33 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int sum=0,min=Integer.MAX_VALUE;
-        int i=0,j=0;
-        for(;i<nums.length;i++){
-            sum+=nums[i];
-            if(sum>=target){
-                //min=Math.min(min,i-j+1);
-                while(sum>=target){
-                    sum-=nums[j];
-                    j++;
-                }
-                min=Math.min(min,i-j+2);
-            }
+        int n=nums.length;
+        long[] pre=new long[n+1];
+        pre[0]=0;
+        for(int i=1;i<=n;i++){
+            pre[i]=nums[i-1]+pre[i-1];
         }
-        if(min==Integer.MAX_VALUE)return 0;
-        return min;
+        // for(int i=0;i<=n;i++)
+        // System.out.print(pre[i]+" ");
+        int min=1000000;
+        for(int i=0;i<=n;i++){
+            long lsum=pre[i];
+            long rsum=lsum+target;
+            int l=i,r=n;
+            int ind=-1;
+            while(l<=r){
+                
+                int m=l+(r-l)/2;
+                //System.out.println(m+" "+pre[m]);
+                if(pre[m]>=rsum){
+                    ind=m;
+                    r=m-1;
+                }
+                else l=m+1;
+            }
+            //System.out.println(i+" "+ind);
+            if(ind!=-1)
+            min=Math.min(min,ind-i);
+        }
+        return min==1000000?0:min;
     }
 }
