09f2618e2fa6acc8555835f75d01b0c6831758a0
Prashant Kumar <60420825+Orewa-Prashant@users.noreply.github.com>
2022-07-06 21:55:38 +0530

    Time: 10 ms (47.09%), Space: 161.5 MB (73.88%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,31 +1,21 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> less=new ArrayList<>();
-        List<Integer> more=new ArrayList<>();
-        List<Integer> equal = new ArrayList<>();
-        
+        int n=nums.length;
         int eq=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==pivot)
-            {
-                equal.add(nums[i]);
-            }
-            else if(nums[i] > pivot)
-            {
-                more.add(nums[i]);
-            }
-            else
-                less.add(nums[i]);
+        int less=0;
+        for(int i:nums){
+            if(i<pivot)less++;
+            else if(i==pivot)eq++;
         }
-        
-        int result[] = new int[nums.length];
-        int k = 0;
-        for(Integer itr: less)
-            result[k++] = itr;
-        for(Integer itr: equal)
-            result[k++] = itr;
-        for(Integer itr: more)
-            result[k++] = itr;
-        return result;
+        int[] res=new int[n];
+        int k=0;
+        int more=less+eq;
+        int equal=less;
+        for(int i:nums){
+            if(i<pivot)res[k++]=i;
+            else if(i==pivot)res[equal++]=i;
+            else res[more++]=i;
+        }
+        return res;
     }
 }
