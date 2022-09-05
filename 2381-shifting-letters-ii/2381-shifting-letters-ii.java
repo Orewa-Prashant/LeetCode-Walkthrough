e8a65a7adcfa51c764fcbbd4adccc17a08c4e3f6
Prashant Kumar <60420825+Orewa-Prashant@users.noreply.github.com>
2022-09-05 19:32:06 +0530

    Time: 20 ms (27.14%), Space: 103.8 MB (68.91%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.java
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.java
@@ -18,26 +18,15 @@ class Solution {
         for(int i=1;i<s.length();i++){
             pre[i]=pre[i-1]+arr[i];
         }
-        //for(int i=0;i<s.length();i++){
-            //System.out.println(pre[18]);
-        //}
-        // System.out.println();
-        // return "";
         for(int i=0;i<s.length();i++){
             if(pre[i]==0)continue;
             char ch=sb.charAt(i);
             int val=ch-96;
             int inc=pre[i]%26;
-            // if(i==18)
-            // System.out.println(inc);
             if(inc<0)inc+=26;
-            // if(i==18)
-            // System.out.println(inc+" "+val);
             val+=inc;
             val%=26;
             val=val==0?26:val;
-            // if(i==18)
-            // System.out.println(val);
             sb.setCharAt(i,(char)(96+val));
         }
         return sb.toString();
