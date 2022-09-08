e671ec898694d9f9679a3487fab5edc6550858b8
Prashant Kumar <60420825+Orewa-Prashant@users.noreply.github.com>
2022-09-08 15:00:01 +0530

    Time: 181 ms (62.28%), Space: 193.5 MB (33.21%) - LeetHub

---
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.java
@@ -0,0 +1,19 @@
+class Solution {
+    int m=1000000007;
+    //Map<String,Integer> map=new HashMap<>();
+    
+    public int numberOfWays(int startPos, int endPos, int k) {
+        Integer[][] dp=new Integer[4002][k+1];
+        
+        return fun(startPos,endPos,k,dp);
+    }
+    int fun(int s, int e, int k, Integer[][] dp){
+        if(Math.abs(s-e)>k)return 0;
+        if(k==0){
+            return s==e?1:0;
+        }
+        if(dp[s+1000][k]!=null)return dp[s+1000][k];
+        return dp[s+1000][k]=(fun(s+1,e,k-1,dp)%m+fun(s-1,e,k-1,dp)%m)%m;
+        
+    }
+}
