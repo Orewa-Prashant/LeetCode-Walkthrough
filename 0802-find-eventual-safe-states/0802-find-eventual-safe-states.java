3a63bf6413412f702835388dcaa5190725ae0409
Prashant Kumar <60420825+Orewa-Prashant@users.noreply.github.com>
2022-12-24 15:39:14 +0530

    Time: 16 ms (52.75%), Space: 51.4 MB (80.39%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -0,0 +1,33 @@
+class Solution {
+    Set<Integer> set = new HashSet<>();
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        
+        int n=graph.length;
+        boolean[] visited = new boolean[n];
+        boolean[] pathVisited = new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(i,graph,visited, pathVisited);
+            }
+            // System.out.println(set);
+        }
+        List<Integer> ans = new ArrayList<>(set);
+        Collections.sort(ans);
+        return ans;
+    }
+    boolean dfs(int node, int[][] graph, boolean[] visited, boolean[] pathVisited){
+        if(pathVisited[node])return false;
+        // System.out.println(node);
+        pathVisited[node] = true;
+        for(int next : graph[node]){
+            if(set.contains(next)){
+                continue;
+            }
+            if(!dfs(next, graph, visited, pathVisited))return false;            
+        }
+        set.add(node);
+        // System.out.println(node+"---"+set);
+        pathVisited[node] = false;
+        return true;
+    }
+}
