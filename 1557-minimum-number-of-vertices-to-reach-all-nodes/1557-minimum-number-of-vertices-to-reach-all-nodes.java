5f1a1350bf3741847e788126ceadfd33e1e7bd50
Prashant Kumar <60420825+Orewa-Prashant@users.noreply.github.com>
2023-05-19 15:03:09 +0530

    Time: 12 ms (41.73%), Space: 79 MB (73.96%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
         List<Integer> ans = new ArrayList<>();
-        Map<Integer, Integer> inDegreeCount = new HashMap<>();
-        for(int i=0;i<n;i++) {
-            inDegreeCount.put(i, 0);
-        }
+        int[] inDegreeCount = new int[n];
+        
         for(int i=0;i<edges.size();i++) {
-            inDegreeCount.put(edges.get(i).get(1), inDegreeCount.getOrDefault(edges.get(i).get(1), 0)+1);
+            inDegreeCount[edges.get(i).get(1)]++;
         }
         List<Integer> result = new ArrayList<>();
-        for(Integer itr: inDegreeCount.keySet()) {
-            if(inDegreeCount.get(itr)==0) {
-                result.add(itr);
+        int index = 0;
+        for(Integer itr: inDegreeCount) {
+            
+            if(itr==0) {
+                result.add(index);
             }
+            index++;
         }
         return result;
         
